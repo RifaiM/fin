896675149a38f109a318ba1ff2fdfb82cce3d47c
DPR - <rifaialc@gmail.com>
2021-11-22 03:17:20 +0700

    add btn audio

--- js/audio.js
+++ js/audio.js
@@ -1,5 +1,18 @@
-    window.addEventListener("DOMContentLoaded", event => {
-        const audio = document.querySelector("audio");
-        audio.volume = 1.0;
-        audio.play();
+const button = document.querySelector("#sound");
+const icon = document.querySelector("#sound > i");
+const audio = document.querySelector("audio");
+
+button.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.volume = 0.2;
+    audio.play();
+    icon.classList.remove('bi-volume-up');
+    icon.classList.add('bi-volume-mute');
+    
+  } else {
+    audio.pause();
+    icon.classList.remove('bi-volume-mute');
+    icon.classList.add('bi-volume-up');
+  }
+  button.classList.add("fade");
 });
